ae1711e687b1232aef39007e4b937ab782915ee0
Sameh Magdy <thesamehmagdy@gmail.com>
2019-01-22 16:17:49 +0000

    Configure Passport

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express          = require("express"),
     app              = express(),
     mongoose         = require("mongoose"),
     bodyParser       = require("body-parser"),
+    passport         = require("passport"),
+    LocalStrategy    = require("passport-local"),
+    jwt              = require('jsonwebtoken'),
+    passportJWT      = require("passport-jwt"),
+    JWTStrategy      = passportJWT.Strategy,
+    ExtractJWT       = passportJWT.ExtractJwt,
     Pirate           = require("./models/pirate");
 
 require('dotenv').config();
@@ -15,6 +21,42 @@ mongoose.connect(url, {useNewUrlParser: true});
 // Use bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Express Session
+app.use(require("express-session")({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport Configuration 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport Local
+passport.use(new LocalStrategy(
+  User.authenticate()
+));
+
+// Passport JWT
+passport.use(new JWTStrategy({
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey   : process.env.JWT_SECRET
+    },
+    function (jwtPayload, cb) {
+        return User.findById(jwtPayload.id)
+            .then(user => {
+                return cb(null, user);
+            })
+            .catch(err => {
+                return cb(err);
+            });
+    }
+));
+
+// Passport sessions
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Get All Pirates
 app.get("/pirates", function(req, res, next) {
